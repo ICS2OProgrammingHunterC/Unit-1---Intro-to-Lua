55c8aeb0bbc3c09be5107d39bf88c1f05b61cbfa
Hunter Connolly <hunter.connolly@ocsbstudent.ca>
2019-10-02 13:56:30 -0400

    updated

--- TouchAndDragHunter/main.lua
+++ TouchAndDragHunter/main.lua
@@ -8,20 +8,21 @@ display.setStatusBar(display.HiddenStatusBar)
 
 --loacal variables
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
-local appleLogo = display.newImageRect("Images/Apple.png", 150,150)
+local appleLogo = display.newImageRect("Images/apple.png", 150,150)
 local appleLogoWidth = appleLogo.width
 local appleLogoHeight = appleLogo.height
 
-local macbook = display.newImageRect("Images/macbook.png", 150, 150)
-local macbookHeight = macbook.height
-local macbookWidth = macbook.width
+local Girl = display.newImageRect("Images/girl2.png", 150, 150)
+local GirlHeight = girl.height
+local GirlWidth = girl.width
 
 --my boolean variables to keep track of which object I touched first
 local alreadyTouchedAppleLogo = false
-local alreadyTouchedMacbook = false
+local alreadyTouchedGirl = false
 
 --set the initial x and y position of the images
 appleLogo.x = 400
 appleLogo.y = 500
 
-macbook.x = 300
+Girl.x = 300
+Girl.y = 500
