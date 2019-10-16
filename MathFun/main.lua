00e13db8c913a0844785b8619c15318c45e5c2d4
Hunter Connolly <hunter.connolly@ocsbstudent.ca>
2019-10-16 14:31:30 -0400

    yeet

--- MathFun/main.lua
+++ MathFun/main.lua
@@ -23,7 +23,8 @@ local pointsText
 local lives = 3
 local livesText
 local divQuestion
-
+local youWin
+local youLose
 ----------------------------------------------------------------------------------
 -- SOUNDS
 ---------------------------------------------------------------------------------------
@@ -35,6 +36,13 @@ local correctSoundChannel
 local incorrectSound = audio.loadSound( "Sounds/Wrong Buzzer Sound.mp3")
 local incorrectSoundChannel
 
+-- you lose sound
+local youLoseSound = audio.loadSound("Sounds/YouLose.mp3")
+local youLoseSoundChannel
+
+--you win sound
+local youWinSound = audio.loadSound("Sounds/youWin.mp3")
+local youWinSoundChannel
 ---------------------------------------------------------------------------------
 --LOCAL FUNCTIONS
 -----------------------------------------------------------------------------------
@@ -46,8 +54,6 @@ local function AskQuestion()
 	--generate 4 random numbers
 	randomNumber1 = math.random(1,10)
 	randomNumber2 = math.random(1,10)
-	randomNumber3 = math.random(2,12)
-	randomNumber4 = math.random(2,12)
 
 	--if the random operator is 1 then it is addition
 	if (randomOperator == 1) then
@@ -63,14 +69,15 @@ local function AskQuestion()
 
 		-- create the question in text object
 		questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
-	end
-	if (randomOperator == 3) then
+	elseif (randomOperator == 3) then
 		--calculate the correct answer
 		correctAnswer = randomNumber1 * randomNumber2
 
 		-- create the question in text object
 		questionObject.text = randomNumber1 .. " x " .. randomNumber2 .. " = "
 	elseif (randomOperator == 4) then
+		randomNumber3 = math.random(2,12)
+		randomNumber4 = math.random(2,12)
 		-- find the answer
 		divQuestion = randomNumber3 * randomNumber4
 		correctAnswer = divQuestion / randomNumber3 
@@ -90,6 +97,16 @@ end
 
 local function HideIncorrect()
 	incorrectObject.isVisible = false
+	AskQuestion()
+end
+
+local function HideRedX()
+	redX.isVisible = false
+	AskQuestion()
+end
+
+local function HideCheckmark()
+	checkmark.isVisible = false
 	AskQuestion()
 end
 
@@ -126,15 +143,31 @@ local function NumericFieldListener( event )
 			-- update it in the display object
 			livesText.text = "Lives = " .. lives
 			--it tells the user that the answer is incorrect
-			incorrectObject.isVisible = true
 			timer.performWithDelay(1500, HideIncorrect)
 			--play the incorrect sound
 			incorrectSoundChannel = audio.play(incorrectSound)
 			--clear the text field
 			event.target.text = ""
+			--display the x 
+			redX.isVisible = true
+			timer.performWithDelay(1500, HideRedX)
+			incorrectObject = display.newText( "Incorrect, the correct answer is " .. correctAnswer  , display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+			incorrectObject:setTextColor(1,0,1)
+			
+
 		end
 		if (lives == 0) then
-			
+			youLose = display.newImageRect("Images/YouLose.png", 1024, 768)
+			youLose.x = display.contentWidth/2
+			youLose.y = display.contentHeight/2
+			numericField.isVisible = false
+			youLoseSoundChannel = audio.play(youLoseSound)
+		elseif (points == 5)then
+			youWin = display.newImageRect("Images/Youwin.jpg", 1024, 768)
+			youWin.x = display.contentWidth/2
+			youWin.y = display.contentHeight/2
+			numericField.isVisible = false	
+			youWinSoundChannel = audio.play(youWinSound)
 		end
 	end	
 end
@@ -154,9 +187,7 @@ correctObject:setTextColor(1,0,1)
 correctObject.isVisible = false
 
 -- create the incorrect object
-incorrectObject = display.newText( "Incorrect, the correct answer is" .. correctAnswer , display.contentWidth/2, display.contentHeight*2/3, nil, 50)
-incorrectObject:setTextColor(1,0,1)
-incorrectObject.isVisible = false
+
 
 
 --create numeric field
@@ -179,8 +210,10 @@ checkmark = display.newImageRect("Images/checkmark.png", 150, 150)
 checkmark.isVisible = false
 
 -- create the red x for when the user get's the answer incorrect
-redX = display.newImageRect("Images/red_x.png", 150, 150)
+redX = display.newImageRect("Images/red_x.png", 150, 150 )
 redX.isVisible = false
+redX.x = display.contentWidth*3/4
+redX.y = display.contentHeight/2 
 
 --------------------------------------------------------------------------------------
 -- FUNCTION CALLS
