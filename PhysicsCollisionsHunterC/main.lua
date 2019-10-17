2b19614bbcc9521e8809a0e665cd909e7ee04034
Hunter Connolly <hunter.connolly@ocsbstudent.ca>
2019-10-17 14:33:48 -0400

    yeet

--- PhysicsCollisionsHunterC/main.lua
+++ PhysicsCollisionsHunterC/main.lua
@@ -1,7 +1,24 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Physics and Collisions
+-- Name: Hunter C
+-- Course: ICS2O
+-----------------------------------------------------------------------------------------------------------------------------
+display.setStatusBar(display.HiddenStatusBar)
 
--- Your code here
+-- load physics
+local physics = require("physics")
+
+-- start physics
+physics.start()
+-------------------------------------------------------------------------------------------
+-- OBJECTS
+----------------------------------------------------------------------------------------
+-- Ground
+local ground = display.newImage("Images/ground.png", 0, 0)
+	ground.x = display.contentWidth/2
+	ground.y = display.contentHeight*7.9/8
+
+	--change the width to be the same as the screen
+	ground.width = display.contentWidth
+
+	-- add the physics
+	physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
